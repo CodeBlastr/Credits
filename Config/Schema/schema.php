44acbab7701cfa7b57d4c5159b6d9e52dbf655ba
Richard Kersey <github@razorit.com>
2012-05-08 00:01:56 -0400

    Mostly testing the upgrades, and updating schemas, but includes a folder rename left over with lower case from cake 2.0 upgrade

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -1,24 +1,34 @@
 <?php 
-/* generated on: 2012-04-10 18:40:47 : 1334083247 */
 class CreditsSchema extends CakeSchema {
 
+	public $renames = array();
+
+	public function __construct($options = array()) {
+		parent::__construct();
+	}
+
 	public function before($event = array()) {
-		return true;
+		App::uses('UpdateSchema', 'Model'); 
+		$this->UpdateSchema = new UpdateSchema;
+		$before = $this->UpdateSchema->before($event);
+		return $before;
 	}
 
 	public function after($event = array()) {
+		$this->UpdateSchema->rename($event, $this->renames);
+		$this->UpdateSchema->after($event);
 	}
-	
+
 	public $credits = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary', 'collate' => NULL, 'comment' => ''),
-		'credit_type_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10, 'collate' => NULL, 'comment' => 'Allows a site to have multiple credit types with different values.  Type values will be a system setting.'),
-		'user_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
-		'amount' => array('type' => 'float', 'null' => false, 'default' => '0', 'collate' => NULL, 'comment' => ''),
-		'value' => array('type' => 'float', 'null' => true, 'default' => '0', 'collate' => NULL, 'comment' => 'A convenience and performance field for easily finding the dollar value of this credit record.'),
-		'creator_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
-		'modifier_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
-		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
-		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
+		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'credit_type' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'comment' => 'Allows a site to have multiple credit types with different values.  Type values will be a system setting.', 'charset' => 'utf8'),
+		'user_id' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'amount' => array('type' => 'float', 'null' => false, 'default' => '0'),
+		'value' => array('type' => 'float', 'null' => true, 'default' => '0', 'comment' => 'A convenience and performance field for easily finding the dollar value of this credit record.'),
+		'creator_id' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'modifier_id' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
 	);
